6e9e028c3d00dd51f8f5d9179b20d2aa6615fae7
Michael Blatter <mblatter@spartzinc.com>
2014-12-16 14:58:08 -0600

    Docs

--- docs/examples/ios/README.md
+++ docs/examples/ios/README.md
@@ -1,18 +1,21 @@
-## Android Example
+## iOS Example
 
 This example is to show how you can use AutomationObject framework to automate apps.  I also recommend you use their
 GUI application to grab all the xPaths and layout your blueprints from their.
+
+I did not automate the entire app, just wanted to give you an idea on how to automate iOS apps.
 
 __Important__: You will need to start the Appium server on port 4723 which is the default port.  If you are unfamiliar
  with their node setup you can use their GUI application [here](http://appium.io/).
 
 __Built Using__:
-*  Android API 17
+*  xCode 6
 *  Appium 1.3.3
+*  iPhone 6
 
 Some Additional Links:
-[Appium Android Documentation](https://github.com/appium/ruby_lib/blob/master/docs/android_docs.md)
-[Sample Android App Used](https://github.com/stephanenicolas/RoboDemo)
+[Appium iOS Documentation](https://github.com/appium/ruby_lib/blob/master/docs/ios_docs.md)
+[Sample iOS App Used](https://github.com/aryaxt/iOS-Slide-Menu)
 
 ### Installation and Execution
 
---
+++ docs/examples/ios/run.rb
@@ -0,0 +1,56 @@
+require 'automation_object'
+require 'appium_lib'
+
+#Change to true if you want to see the debug output in the console
+AutomationObject::debug_mode = true
+AutomationObject::BluePrint::base_directory = File.expand_path(File.dirname(__FILE__))
+
+#Start up Appium driver on port 4723
+opts = { caps: { platformName: :ios, deviceName: 'iPhone 6',
+                 app: File.join(File.expand_path(File.dirname(__FILE__)), '/SlideMenu.ipa') } }
+Appium::Driver.new(opts).start_driver
+#Appium sets itself as $driver
+
+begin
+  blue_prints = AutomationObject::BluePrint.new('blue_prints')
+  automation_object = AutomationObject::Framework.new($driver, blue_prints)
+
+  #Login and go to the home screen
+  automation_object.login_screen.username_field.send_keys('blah')
+  automation_object.login_screen.password_field.send_keys('blah')
+  automation_object.login_screen.login_button.click
+
+  #Go back and forth
+  automation_object.home_screen.menu_button.click
+  automation_object.menu_screen.menu_button.click
+
+  #Then lets test automatic screen routing
+  15.times do
+    random_number = rand(0..4)
+
+    case random_number
+      when 0
+        automation_object.friends_screen
+      when 1
+        automation_object.home_screen
+      when 2
+        automation_object.login_screen
+      when 3
+        automation_object.menu_screen
+      when 4
+        automation_object.profile_screen
+    end
+  end
+
+
+
+
+
+
+
+
+ensure
+  puts 'Press enter to quit...'
+  input = gets
+  driver.quit
+end
